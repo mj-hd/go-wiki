1135e59371ee2e1bb244a83889656b05ff234ef5
mjhd <mjhd@devlion.net>
2014-07-20 22:24:47 +0900

    configファイルを追加

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+const (
+	ServerPort string = "8080"
+	DBHost     string = "192.168.3.3"
+	DBPort     string = "3306"
+	DBName     string = "go_wiki"
+	DBUser     string = "go_wiki"
+	DBPass     string = "example_password"
+)
