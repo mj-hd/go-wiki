fab52da874a78e09421d0b8194049fcde2663fc1
mjhd <mjhd@devlion.net>
2014-07-27 10:19:50 +0900

    form.jsの不具合を修正

--- static/js/form.js
+++ static/js/form.js
@@ -1,22 +1,26 @@
 $(function() {
 
-	// a-zA-Z0-9, 文字数など
-	$(".name").bind("keyup", function() {
-		if ($(this).val().length < 1) {
-			$(this).parent("tr").append("<td>名前を入力してください。</td>")
-		}
-	});
 
-	$(".address").bind("keyup", function() {
-		if (!$(this).val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)){
-			$(this).parent("tr").append("<td>アドレスが正しくありません。</td>");
-		}
-	});
+	$("form").submit(function() {
 
-	$(".password").bind("keyup", function() {
-		if (($(this).val().length < 1) || ($(this).val().length > 50)) {
-			$(this).parent("tr").append("<td>パスワードは1文字以上、50文字以内にしてください。</td>")
+		var hasErrorOccured = false;
+
+		$(".error").remove()
+
+		// a-zA-Z0-9, 文字数など
+		if ($(".name").val().length < 1) {
+			$(".name").parent().append("<td class='error'>名前を入力してください。</td>");
+			hasErrorOccured = true;
+		}	
+		if (!$(".address").val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)){
+			$(".address").parent().append("<td class='error'>アドレスが正しくありません。</td>");
+			hasErrorOccured = true;
+		}	
+		if (($(".password").val().length < 1) || ($(this).val().length > 50)) {
+			$(".password").parent().append("<td class='error'>パスワードは1文字以上、50文字以内にしてください。</td>");
+			hasErrorOccured = true;
 		}
+		return !hasErrorOccured;
 	});
 
 });
