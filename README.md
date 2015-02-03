163a7c9062fc66aa7b7dc09e05205223547c206d
mjhd <mjhd@devlion.net>
2015-02-03 16:03:31 +0900

    Add function that deamonize go-wiki

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # go-wiki
 タイトルは仮称。Markdownを使った、golangによるWikiシステム。
+
+# インストール
+config/config.goを作成し、config.go.exampleを参考にして設定する。
+設定が終わったら、go build go-wiki.goを実行する。
+
+init.dを使用するシステムの場合は、config/init_scriptをシンボリックリンクとして/etc/init.d/以下に貼る。
+そして、/etc/init.d/go-wiki startを実行することで、起動できる。
--- go-wiki.go
+++ go-wiki.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
+	"syscall"
 
 	"go-wiki/config"
 	"go-wiki/controllers"
@@ -15,6 +17,15 @@ import (
 )
 
 func main() {
+
+	errcd := daemon(0, 0)
+
+	if errcd != 0 {
+		fmt.Println("デーモン化に失敗。")
+		os.Exit(1)
+	}
+
+	os.Chdir(config.GowikiPath)
 
 	utils.LogFile = config.LogFile
 	utils.DisplayLog = config.DisplayLog
@@ -37,4 +48,55 @@ func main() {
 
 	utils.PromulgateInfoStr(os.Stdout, "ポート"+config.ServerPort+"でサーバを開始...")
 	http.ListenAndServe(":"+config.ServerPort, context.ClearHandler(http.DefaultServeMux))
+}
+
+/*
+daemon関数
+*/
+func daemon(nochdir, noclose int) int {
+ var ret uintptr
+ var err syscall.Errno
+ 
+ // バックグラウンドプロセスにする
+ // 子プロセスを生成し，親プロセスを終了する
+ ret, _, err = syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+ if err != 0 {
+  return -1
+ }
+ switch ret {
+  case 0:
+   // 子プロセスが生成できたらそのまま処理を続ける
+   break
+  default:
+   // 親プロセスだとここで終了する
+   os.Exit(0)
+ }
+ 
+  
+ // 新しいセッションを生成(子プロセスがセッションリーダになる)
+ pid, _ := syscall.Setsid()
+ if pid == -1 {
+  return -1
+ }
+  
+ if nochdir == 0 {
+  // カレントディレクトリの再設定。ここではルートにしておく
+  os.Chdir("/")
+ }
+  
+ // ファイルのパーミッションを再設定(必須ではない。オプション)
+ syscall.Umask(0)
+ 
+ if noclose == 0 {
+  // 標準入出力先を/dev/nullファイルに変更して、すべて破棄する
+  f, e := os.OpenFile("/dev/null", os.O_RDWR, 0)
+  if e == nil {
+   fd := int(f.Fd())
+   syscall.Dup2(fd, int(os.Stdin.Fd()))
+   syscall.Dup2(fd, int(os.Stdout.Fd()))
+   syscall.Dup2(fd, int(os.Stderr.Fd()))
+  }
+ }
+ 
+ return 0
 }
